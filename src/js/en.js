d6fe37e4fdcbf6d8bb54d4dc26d85bcd69914b2a
qixiang <qixiang1009@gmail.com>
2023-01-16 04:21:49 +0800

    Updated. 2023/01/16

---
+++ src/js/en.js
@@ -0,0 +1,26 @@
+const content_EN = {
+
+    "nav-link-1":"About",
+    "nav-link-2":"Education & Experience",
+    "nav-link-3":"Skills & Languages",
+    "nav-link-4":"Awards & Interests",
+    "nav-link-5":"My Blog",
+    "carousel-title1": "2022 NCU MIS Carnival",
+    "carousel-title2": "2022 NCU MIS Carnival",
+    "carousel-title3": "2022 NCU MIS Carnival",
+    "card-name": "QI-XIANG, ZHANG",
+    "card-school": "MIS in International",
+    "card-department": "Information & System",
+    "card-location": "Taoyuan, Taiwan",
+    "education_university": "National Central University",
+    "education_department": "Management in Information System",
+    "education_highschool": "Taichung First Senior High School",
+    "experience1": "Glory eLearning Website Manager",
+    "experience2": "NCU Traditional Chinese Music Club, Member",
+    "experience3": "NCU Traditional Chinese Music Club, Erhu Group Leader",
+    "experience4": "NCU Coding Club, Member",
+    "experience5": "NCU MIS Carnival, Curation Group Member",
+
+}
+
+export {content_EN};
---
+++ src/js/zh-TW.js
@@ -0,0 +1,25 @@
+const content_zhTW = {
+
+    "nav-link-1":"關於",
+    "nav-link-2":"學歷 & 經驗",
+    "nav-link-3":"技能 & 語言",
+    "nav-link-4":"獎項 & 興趣",
+    "nav-link-5":"個人部落格",
+    "carousel-title1": "2022 中央資管嘉年華",
+    "carousel-title2": "2022 中央資管嘉年華",
+    "carousel-title3": "2022 中央資管嘉年華",
+    "card-name": "張棨翔",
+    "card-school": "國立中央大學",
+    "card-department": "資訊管理學系",
+    "card-location": "桃園, 台灣",
+    "education_university": "國立中央大學",
+    "education_department": "資訊管理學系",
+    "education_highschool": "臺中市立臺中第一高級中學",
+    "experience1": "榮耀數位學習 網站管理員",
+    "experience2": "國立中央大學 國樂社 成員",
+    "experience3": "國立中央大學 國樂社 二胡組 組長",
+    "experience4": "國立中央大學 程式設計研究社 成員",
+    "experience5": "國立中央大學 資管嘉年華 策展組",
+}
+
+export {content_zhTW};
