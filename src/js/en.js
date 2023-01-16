168ae06fcb7e3e546236025cf5b5a7c9bf5c7fea
qixiang <qixiang1009@gmail.com>
2023-01-17 05:46:40 +0800

    Updated. 2022/01/17

--- src/js/en.js
+++ src/js/en.js
@@ -1,10 +1,14 @@
 const content_EN = {
 
+    "home": "Home",
+    "offcanvasNavbarLabel": "Welcome to my website!",
+
     "nav-link-1":"About",
     "nav-link-2":"Education & Experience",
     "nav-link-3":"Skills & Languages",
-    "nav-link-4":"Awards & Interests",
+    "nav-link-4":"Achievements & Interests",
     "nav-link-5":"My Blog",
+    
     "carousel-title1": "2022 NCU MIS Carnival",
     "carousel-title2": "2022 NCU MIS Carnival",
     "carousel-title3": "2022 NCU MIS Carnival",
@@ -12,14 +16,32 @@ const content_EN = {
     "card-school": "MIS in International",
     "card-department": "Information & System",
     "card-location": "Taoyuan, Taiwan",
-    "education_university": "National Central University",
-    "education_department": "Management in Information System",
+    "about_title": "About",
+    
+    "education_title": "Education",
+    "education_program": "National Central University, ERP Center<br>Business Intelligence & Analytics Program",
+    "education_university": "National Central University<br>Management in Information System",
     "education_highschool": "Taichung First Senior High School",
+    
+    "experience_title": "Experience",
     "experience1": "Glory eLearning Website Manager",
     "experience2": "NCU Traditional Chinese Music Club, Member",
     "experience3": "NCU Traditional Chinese Music Club, Erhu Group Leader",
     "experience4": "NCU Coding Club, Member",
     "experience5": "NCU MIS Carnival, Curation Group Member",
+    
+    "achievements_title": "Achievements",
+    "achievement1": "The 14th ITHELP Ironman Contest 2022<br>《The Journey of Django Starter》",
+    "achievement2": "The 13th ITHELP Ironman Contest 2021<br>《JS30 Learning Diary》",
+    "achievement3": "Google Developers Android App Study Jam 2021",
+    "achievement4": "JLPT N4 Certificate",
+
+    "interests_title": "Interests",
+    "interest1": "Animation&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Reading",
+    "interest2": "Coding&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Web Design",
+    "interest3": "Stock&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Computer Science",
+
+    "contact_title": "Contact",
 
 }
 
--- src/js/zh-TW.js
+++ src/js/zh-TW.js
@@ -1,10 +1,14 @@
 const content_zhTW = {
 
+    "home": "首頁",
+    "offcanvasNavbarLabel": "歡迎來到我的網站!",
+
     "nav-link-1":"關於",
-    "nav-link-2":"學歷 & 經驗",
+    "nav-link-2":"學歷 & 經歷",
     "nav-link-3":"技能 & 語言",
-    "nav-link-4":"獎項 & 興趣",
+    "nav-link-4":"成就 & 興趣",
     "nav-link-5":"個人部落格",
+    
     "carousel-title1": "2022 中央資管嘉年華",
     "carousel-title2": "2022 中央資管嘉年華",
     "carousel-title3": "2022 中央資管嘉年華",
@@ -12,14 +16,33 @@ const content_zhTW = {
     "card-school": "國立中央大學",
     "card-department": "資訊管理學系",
     "card-location": "桃園, 台灣",
-    "education_university": "國立中央大學",
-    "education_department": "資訊管理學系",
+    "about_title": "關於",
+    
+    "education_title": "學歷",
+    "education_program": "國立中央大學, ERP 中心<br>商業智慧與分析學分學程",
+    "education_university": "國立中央大學 資訊管理學系",
     "education_highschool": "臺中市立臺中第一高級中學",
+    "experience_title": "經歷",
+    
     "experience1": "榮耀數位學習 網站管理員",
     "experience2": "國立中央大學 國樂社 成員",
     "experience3": "國立中央大學 國樂社 二胡組 組長",
     "experience4": "國立中央大學 程式設計研究社 成員",
     "experience5": "國立中央大學 資管嘉年華 策展組",
+    
+    "achievements_title": "成就",
+    "achievement1": "2022 iThome 鐵人賽<br>《Django初心者之旅》",
+    "achievement2": "2021 iThome 鐵人賽<br>《JS30學習日記》",
+    "achievement3": "Google Android App 開發培訓計劃 2021",
+    "achievement4": "新日檢 JLPT N4 認證",
+    
+    "interests_title": "興趣",
+    "interest1": "動畫&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;閱讀",
+    "interest2": "寫程式&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;網頁設計",
+    "interest3": "股票投資&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;電腦科學",
+
+    "contact_title": "聯絡我",
+
 }
 
 export {content_zhTW};
