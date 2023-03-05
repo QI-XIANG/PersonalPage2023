5d84294c1362347358be2b520d5a7050a4309c18
qixiang <qixiang1009@gmail.com>
2023-03-06 01:28:00 +0800

    fix some layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,32 @@
 //載入語系包
-import { content_zhTW } from './zh-TW.js';
-import { content_EN } from './en.js';
-console.log(content_zhTW)
-console.log(content_EN)
+import { content_zhTW } from "./zh-TW.js";
+import { content_EN } from "./en.js";
+console.log(content_zhTW);
+console.log(content_EN);
 
 //console.log(document.querySelectorAll('.translation a'))
-var translation = document.querySelector('.translation a');
+var translation = document.querySelector(".translation a");
 
+document.querySelectorAll(".translation-selection a").forEach((link) => {
+  link.addEventListener("click", (e) => {
+    console.log(link.innerHTML);
+    translation.innerHTML = link.innerHTML;
+    if (link.innerHTML == "中文") {
+      Object.keys(content_zhTW).forEach((key) => {
+        document.querySelector("#" + key).innerHTML = content_zhTW[key];
+      });
+    } else {
+      Object.keys(content_EN).forEach((key) => {
+        document.querySelector("#" + key).innerHTML = content_EN[key];
+      });
+    }
+  });
+});
 
-document.querySelectorAll('.translation-selection a').forEach(link => {
-    link.addEventListener('click', e=>{
-        console.log(link.innerHTML);
-        translation.innerHTML = link.innerHTML;
-        if(link.innerHTML == '中文'){
-            Object.keys(content_zhTW).forEach(key => {
-                document.querySelector('#'+key).innerHTML = content_zhTW[key];
-            })
-        }else{
-            Object.keys(content_EN).forEach(key => {
-                document.querySelector('#'+key).innerHTML = content_EN[key];
-            })
-        }
-    })
-})
+/*載入中圖示*/
+let preloader = document.querySelector("#preloader");
+if (preloader) {
+  window.addEventListener("load", () => {
+    preloader.remove();
+  });
+}
